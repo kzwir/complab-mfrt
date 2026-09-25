1f6a7b8c72b18879e741dbb0e0d1f54d013f598c
Krzysztof Żwirek <mmakzwi@wp.pl>
2026-09-25 15:04:48 +0200

    Add DashboardDto class for dashboard data transfer

---
+++ src/backend/CompLab.Application/DTOs/Dashboard/DashboardDto.cs
@@ -0,0 +1,10 @@
+namespace CompLab.Application.DTOs.Dashboard;
+
+public sealed class DashboardDto
+{
+    public int MixtureCount { get; init; }
+
+    public int SampleCount { get; init; }
+
+    public int TestCount { get; init; }
+}
