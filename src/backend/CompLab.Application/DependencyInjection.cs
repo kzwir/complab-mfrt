fc7f46c4129900ee879f30c813a6e69d9fd6d3c6
Krzysztof Żwirek <mmakzwi@wp.pl>
2026-09-25 15:07:36 +0200

    Add DependencyInjection class for service registration

---
+++ src/backend/CompLab.Application/DependencyInjection.cs
@@ -0,0 +1,13 @@
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CompLab.Application;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplication(
+        this IServiceCollection services)
+    {
+        return services;
+    }
+}
+``
