dd784eec87eada27ea1872d7d61b6f3b4369e60a
Krzysztof Żwirek <mmakzwi@wp.pl>
2026-09-25 15:28:39 +0200

    Add UpdateMixtureRequest class for mixture updates

---
+++ src/backend/CompLab.Api/Contracts/Mixtures/UpdateMixtureRequest.cs
@@ -0,0 +1,8 @@
+namespace CompLab.Api.Contracts.Mixtures;
+
+public sealed class UpdateMixtureRequest
+{
+    public decimal PolymerPercent { get; init; }
+
+    public decimal QuartzitePercent { get; init; }
+}
